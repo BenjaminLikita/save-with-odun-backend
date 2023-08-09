791a608232bec8c45e74949c80a3f70934fee35d
Harrylever <76135767+Harrylever@users.noreply.github.com>
2023-08-09 15:45:50 +0200

    Changing to production serving

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+import logging
 from mysite import create_app
-
+from waitress import serve
 
 app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    # app.run(debug=True, host="0.0.0.0")
+    logging.basicConfig(level=logging.INFO)
+    serve(app, host='0.0.0.0', port=8080)
